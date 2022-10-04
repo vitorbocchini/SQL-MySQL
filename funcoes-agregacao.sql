ecd8b38dd04d5cb11f5dd6b6875b7179143c4fd4
vitorbocchini <97479423+vitorbocchini@users.noreply.github.com>
2022-10-04 03:56:09 -0300

    Update funcoes-agregacao.sql

--- funcoes-agregacao.sql
+++ funcoes-agregacao.sql
@@ -34,3 +34,17 @@ JOIN customer cus USING(customer_id)
 GROUP BY customer_id
 ORDER BY total DESC
 LIMIT 10;
+
+-- Seleciona os clientes que gastaram mais que $150 e mais de 40 compras.
+SELECT 
+	cus.customer_id AS ID,
+    cus.first_name AS Nome,
+    cus.last_name AS Sobrenome,
+    SUM(amount) AS Total,
+    COUNT(Amount) AS Compras
+FROM payment pay
+JOIN customer cus USING(customer_id)
+GROUP BY customer_id
+HAVING Total >= 150 AND Compras >= 40
+ORDER BY total DESC;
+
